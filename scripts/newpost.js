683f2bc92d4ea170df71ecd6f1c78e9fe64dd333
Seiichi Yonezawa <nzwsch@gmail.com>
2021-09-12 15:46:28 +0900

    Update newpost.js

--- scripts/newpost.js
+++ scripts/newpost.js
@@ -4,8 +4,9 @@ const path = require('path')
 
 function main() {
   const now = DateTime.now().setZone('Asia/Tokyo')
-  const fileName = now.toFormat('yyyyMMdd')
-  const blogPostPath = path.join(__dirname, "../content/blog/", `${fileName}.md`)
+  const fileDir = now.toFormat('yyyyMMdd')
+  const blogPostDir = path.join(__dirname, "../content/blog/", fileDir)
+  const blogPostPath = path.join(blogPostDir, "index.md")
   const blogPostBody = [
     '---',
     'title: "Hello World"',
@@ -13,6 +14,8 @@ function main() {
     `description: "Hello World"`,
     '---'
   ].join("\n")
+
+  fs.mkdirSync(blogPostDir)
 
   fs.writeFile(blogPostPath, blogPostBody, (err) => {
     if (err) throw err
